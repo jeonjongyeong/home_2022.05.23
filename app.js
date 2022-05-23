a37d18bea7ec6f57c3ac94f8c3c85160bd1e9fc1
jkkjkhh <jkkjkhh@gmail.com>
2022-05-23 21:51:46 +0900

    git 셋팅 및 추가

---
+++ app.js
@@ -0,0 +1,181 @@
+// const express = require("express");
+
+import express from "express";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "jkjkhh",
+  password: "sbs123414",
+  database: "a9",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const [rs] = await pool.query(
+    `
+    DELETE 
+    FROM todo
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  res.json({
+    msg: `${id}번 할일이 삭제되었습니다.`,
+  });
+});
+
+app.post("/todos/", async (req, res) => {
+  // const { id } = req.params;
+
+  // const [rows] = await pool.query(
+  //   `
+  //   SELECT *
+  //   FROM todo
+  //   WHERE id = ?
+  //   `,
+  //   [id]
+  // );
+
+  // if (rows.length == 0) {
+  //   res.status(404).json({
+  //     msg: "not found",
+  //   });
+  //   return;
+  // }
+
+  const { reg_date, perform_date, is_completed, content } = req.body;
+
+  // if (!perform_date) {
+  //   res.status(400).json({
+  //     msg: "perform_date required",
+  //   });
+  //   return;
+  // }
+
+  // if (!is_completed) {
+  //   res.status(400).json({
+  //     msg: "is_completed required",
+  //   });
+  //   return;
+  // }
+
+  // if (!content) {
+  //   res.status(400).json({
+  //     msg: "content required",
+  //   });
+  //   return;
+  // }
+
+  const [rs] = await pool.query(
+    `
+    INSERT INTO todo
+    SET reg_date = ?, 
+    perform_date = ?,
+    is_completed = ?,
+    content = ?
+    `,
+    [reg_date, perform_date, is_completed, content]
+  );
+
+  res.json({
+    msg: `할일이 추가되었습니다.`,
+  });
+});
+
+app.patch("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  const { perform_date, is_completed, content } = req.body;
+
+  if (!perform_date) {
+    res.status(400).json({
+      msg: "perform_date required",
+    });
+    return;
+  }
+
+  if (!is_completed) {
+    res.status(400).json({
+      msg: "is_completed required",
+    });
+    return;
+  }
+
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+    UPDATE todo
+    SET perform_date = ?,
+    is_completed = ?,
+    content = ?
+    WHERE id = ?
+    `,
+    [perform_date, is_completed, content, id]
+  );
+
+  res.json({
+    msg: `${id}번 할일이 수정되었습니다.`,
+  });
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    WHERE id = ?
+    `,
+    [id]
+  );
+
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  res.json(rows[0]);
+});
+
+app.get("/todos", async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM article ORDER BY id DESC");
+
+  res.json(rows);
+});
+
+app.listen(port);
